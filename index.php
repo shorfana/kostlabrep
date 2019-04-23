0d28287585bbfb4ffb9c6eb8b6f82c9252dc4da2
iqbal <iqbalshorfana@gmail.com>
2019-04-23 08:13:42 +0700

    start

--- index.php
+++ index.php
@@ -26,10 +26,10 @@
        <input type="submit" name="load_data" value="Load Data" />
  </form>
  <?php
-    $host = "<Nama server database Anda>";
-    $user = "<Nama admin database Anda>";
-    $pass = "<Password admin database Anda>";
-    $db = "<Nama database Anda>";
+    $host = "sb1server";
+    $user = "sb1";
+    $pass = "Muhammad90;
+    $db = "sb1DB";
 
     try {
         $conn = new PDO("sqlsrv:server = $host; Database = $db", $user, $pass);
